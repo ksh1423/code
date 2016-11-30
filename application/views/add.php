2df89f4d406359101c2da9d038eebe0401d7928e
nohseongnam <minuee@nate.com>
2016-11-30 17:14:56 +0900

    add test

--- application/views/add.php
+++ application/views/add.php
@@ -1,8 +1,8 @@
 <script src="/static/lib/ckeditor/ckeditor.js"></script>
 <form action="/index.php/topic/add" method="post" class="span10">
     <?php echo validation_errors(); ?>
-    <input type="text" name="title" placeholder="제목" class="span12" />
-    <textarea name="description" placeholder="본문" class="span12" rows="15" ></textarea>
+    <input type="text" name="title" placeholder="제목" class="span12" value="<?=title?>" />
+    <textarea name="description" placeholder="본문" class="span12" rows="15" value="<?=description?>" ></textarea>
 	<div class="form_control">
 	    <input class="btn" type="submit" />
 	</div>
