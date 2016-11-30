6a0c56ab8e054f6f9c0c72a82109d258957d230c
nohseongnam <minuee@nate.com>
2016-11-30 17:16:29 +0900

    adfsdf

--- application/views/add.php
+++ application/views/add.php
@@ -1,8 +1,8 @@
 <script src="/static/lib/ckeditor/ckeditor.js"></script>
 <form action="/index.php/topic/add" method="post" class="span10">
     <?php echo validation_errors(); ?>
-    <input type="text" name="title" placeholder="제목" class="span12" value="<?=title?>" />
-    <textarea name="description" placeholder="본문" class="span12" rows="15" value="<?=description?>" ></textarea>
+    <input type="text" name="title" placeholder="제목" class="span12" value="<?=$this->input->post('title')?>" />
+    <textarea name="description" placeholder="본문" class="span12" rows="15" value="<?=$this->input->post('description')?>" ></textarea>
 	<div class="form_control">
 	    <input class="btn" type="submit" />
 	</div>
